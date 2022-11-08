0616a67164c63cd4ad81891184706d119bd6e857
Nishant_Srivastav <nishantsrivastav260@gmail.com>
2022-11-08 14:59:31 +0530

    Adding greeting.java plus changes made in the hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,6 @@
 public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello to your Git app");
+        System.out.println("To do multiple git adds at the same time");
     }
 }
