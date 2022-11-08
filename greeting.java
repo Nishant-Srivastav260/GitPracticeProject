43678d6f3210320aa63e50c2880fda7f6db299c5
Nishant_Srivastav <nishantsrivastav260@gmail.com>
2022-11-08 15:17:30 +0530

    Checking git diff...added two sopln statements

--- greeting.java
+++ greeting.java
@@ -1,5 +1,7 @@
 public class Greetings{
     public static void main(String[] args) {
         System.out.println("Greetings!!!");
+        System.out.println("How to know what changes have been made since our last commit");
+        System.out.println("We use  git diff");
     }
 }
