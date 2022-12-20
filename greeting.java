1812f057f7100e032175897d2c0701e063be8596
Nishant_Srivastav <nishantsrivastav260@gmail.com>
2022-12-20 11:40:48 +0530

    Added .gitignore file with Ignore Rules

--- greeting.java
+++ greeting.java
@@ -4,5 +4,7 @@ public class Greetings{
         System.out.println("How to know what changes have been made since our last commit");
         System.out.println("We use  git diff");
         System.out.println("Unstaging Demo");
+       
+	System.out.println("Not needed 1");
     }
 }
